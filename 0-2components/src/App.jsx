f45d74cc792935ca8ed7e328b81a59b62f73483d
Arun Kumar <karun74315@gmail.com>
2026-07-17 10:24:56 +0530

    initial commit

---
+++ 0-2components/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Card from "./components/card";
+
+const App = ()=> {
+  return (
+    <>
+    <Card />
+    </>
+  )
+} 
+export default App;
---
+++ 0-2components/src/components/Card.jsx
@@ -0,0 +1,8 @@
+const Card = () => {    
+return (
+    <div className="card">
+        <h1>Card Component</h1> 
+    </div>
+)
+}   
+export default Card;
