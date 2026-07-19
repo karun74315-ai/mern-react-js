a9d43a34fc42a3f02e3bcac4ef710075543fa0b2
Arun Kumar <karun74315@gmail.com>
2026-07-19 08:19:53 +0530

    props mini project

--- 0-4_props_project/src/App.jsx
+++ 0-4_props_project/src/App.jsx
@@ -1,21 +1,118 @@
 import React from 'react'
+import { Bookmark } from 'lucide-react'
+import Card from './components/Card'
 
 const App = () => {
+  const jobs = [
+    {
+      brandLogo: 'https://logo.clearbit.com/google.com',
+      company: 'Google',
+      datePosted: '5 days ago',
+      post: 'Frontend Developer',
+      tag1: 'Full Time',
+      tag2: 'Senior Level',
+      pay: 45,
+      location: 'Mumbai, India',
+    },
+    {
+      brandLogo: 'https://logo.clearbit.com/microsoft.com',
+      company: 'Microsoft',
+      datePosted: '10 days ago',
+      post: 'React Developer',
+      tag1: 'Part Time',
+      tag2: 'Junior Level',
+      pay: 30,
+      location: 'Bengaluru, India',
+    },
+    {
+      brandLogo: 'https://logo.clearbit.com/amazon.com',
+      company: 'Amazon',
+      datePosted: '2 weeks ago',
+      post: 'UI Engineer',
+      tag1: 'Full Time',
+      tag2: 'Mid Level',
+      pay: 38,
+      location: 'Hyderabad, India',
+    },
+    {
+      brandLogo: 'https://logo.clearbit.com/netflix.com',
+      company: 'Netflix',
+      datePosted: '3 weeks ago',
+      post: 'Product Designer',
+      tag1: 'Contract',
+      tag2: 'Senior Level',
+      pay: 42,
+      location: 'Delhi, India',
+    },
+    {
+      brandLogo: 'https://logo.clearbit.com/spotify.com',
+      company: 'Spotify',
+      datePosted: '1 week ago',
+      post: 'Backend Developer',
+      tag1: 'Full Time',
+      tag2: 'Mid Level',
+      pay: 35,
+      location: 'Pune, India',
+    },
+    {
+      brandLogo: 'https://logo.clearbit.com/meta.com',
+      company: 'Meta',
+      datePosted: '6 days ago',
+      post: 'Mobile App Developer',
+      tag1: 'Full Time',
+      tag2: 'Junior Level',
+      pay: 32,
+      location: 'Chennai, India',
+    },
+    {
+      brandLogo: 'https://logo.clearbit.com/airbnb.com',
+      company: 'Airbnb',
+      datePosted: '4 weeks ago',
+      post: 'Data Analyst',
+      tag1: 'Part Time',
+      tag2: 'Mid Level',
+      pay: 28,
+      location: 'Kolkata, India',
+    },
+    {
+      brandLogo: 'https://logo.clearbit.com/uber.com',
+      company: 'Uber',
+      datePosted: '8 days ago',
+      post: 'DevOps Engineer',
+      tag1: 'Full Time',
+      tag2: 'Senior Level',
+      pay: 40,
+      location: 'Ahmedabad, India',
+    },
+    {
+      brandLogo: 'https://logo.clearbit.com/stripe.com',
+      company: 'Stripe',
+      datePosted: '2 days ago',
+      post: 'QA Engineer',
+      tag1: 'Full Time',
+      tag2: 'Junior Level',
+      pay: 27,
+      location: 'Noida, India',
+    },
+    {
+      brandLogo: 'https://logo.clearbit.com/github.com',
+      company: 'GitHub',
+      datePosted: '9 days ago',
+      post: 'Full Stack Developer',
+      tag1: 'Remote',
+      tag2: 'Mid Level',
+      pay: 33,
+      location: 'Remote, India',
+    },
+  ]
+
   return (
     <div className="Parent">
-
-      <div className="card">
-        <div className="top">
-          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYH2U6iWD6V0vOtxZRruxCaPT98Q94Dl-6EPD8agP3BA&s=10" alt="" />
-          <button>Save</button>
-        </div>
-        <div className="center">
-
-        </div>
-        <div className="bottom">
-          
-        </div>
-      </div>
+      {jobs.map(function(elem){
+        return <Card brandLogo = {elem.brandLogo}
+        company = {elem.company}  post = {elem.post} datePosted = {elem.datePosted}
+        tag1 = {elem.tag1} tag2 = {elem.tag2} pay = {elem.pay} location = {elem.location}   />
+      })}
     </div>
   )
 }
--- 0-4_props_project/src/components/Card.jsx
+++ 0-4_props_project/src/components/Card.jsx
@@ -1,7 +1,30 @@
 const Card = (props) => {
   return (
    <>
-   
+   <div className="card">
+       <div>
+         <div className="top">
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYH2U6iWD6V0vOtxZRruxCaPT98Q94Dl-6EPD8agP3BA&s=10" alt="" />
+          <button>Save  </button>
+        </div>
+        <div className="center">
+         <h3>{props.company} <span>{props.datePosted}</span></h3>
+         <h2>{props.post}</h2>
+         <div className="tag">
+          <h4>{props.tag1}</h4>
+          <h4> {props.tag2}</h4>        
+           </div>
+         
+        </div>
+       </div>
+        <div className="bottom">
+          <div>
+              <h3>{props.pay}</h3>
+              <p>{props.location}</p>
+          </div>
+          <button>Apply Now</button>
+        </div>
+      </div>
    </>
   )
 }   
