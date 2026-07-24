1823444f203f26a3dc1f4e88334341f6d6cfc32f
Arun Kumar <karun74315@gmail.com>
2026-07-24 22:30:15 +0530

    changes in the css files

--- 0-5_css_file/src/App.jsx
+++ 0-5_css_file/src/App.jsx
@@ -1,8 +1,12 @@
 import React from "react";
+import header from "./components/header/Header";
+import Button from "./components/butoon/Button";
 const App = () => {
   return (
     <>
-    
+      <header />
+      <Button />
+      
     </>
   )
 }
---
+++ 0-5_css_file/src/components/header/Header.jsx
@@ -0,0 +1,11 @@
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <h1>this is learning</h1>
+
+    </div>
+  )
+}
+
+export default Header
