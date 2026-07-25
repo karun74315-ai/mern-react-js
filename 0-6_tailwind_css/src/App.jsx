9030956bdd12705166263ef776c5041dae6fef79
Arun Kumar <karun74315@gmail.com>
2026-07-25 17:27:17 +0530

    using tailwind css

---
+++ 0-6_tailwind_css/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <div className="flex justify-center items-center h-screen">
+        <h1 className="text-4xl font-bold text-blue-500 bg-amber-200 h-screen w-full">Hello, Tailwind CSS!</h1>
+      </div>
+    </>
+  );
+};
+
+export default App;
