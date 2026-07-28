9c15bf2281cfd62443d9d1b74f573d6763d40d9b
Arun Kumar <karun74315@gmail.com>
2026-07-28 20:33:14 +0530

    changes in file

--- 0-8_functions/src/App.jsx
+++ 0-8_functions/src/App.jsx
@@ -1,14 +1,42 @@
 import React from 'react'
 
 function App() {
-  const btnClicked = ()=>{
-     console.log("hello");
-     
+  function inputchanging(val){
+    console.log(val);
+    
+  }
+  
+  const pageScroll = ()=>{
+console.log("scrolling .....");
+
   }
   return (
-    <div>
-      <h1>Hello Arun</h1>
-      <button onClick={btnClicked}>change user</button>
+    // <div>
+    //   <h1>Hello Arun</h1>
+    //   <button onClick={()=>{
+    //     console.log("hey");
+        
+    //   }}>change user</button>
+
+    //   <input onChange={(elem)=>{
+    //     inputchanging(elem.target.value)
+        
+    //   }} 
+    //   type="text" placeholder='name' />
+
+    //   <div onMouseMove={(elem)=>{
+    //    console.log(elem.clientY);
+       
+    //   }} 
+    //   id="box">
+    //   </div>
+    // </div>
+
+
+    <div onWheel={pageScroll}>
+      <div className="page1"></div>
+      <div className="page2"></div>
+      <div className="page3"></div>
     </div>
   )
 }
