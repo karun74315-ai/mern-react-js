2a6b2f17238237d2de27dcbcc74040b1f93ba500
Arun Kumar <karun74315@gmail.com>
2026-07-30 15:11:53 +0530

    form handeling

--- 0-11_form_handling/src/App.jsx
+++ 0-11_form_handling/src/App.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 
 function App() {
+
+  const submith = (e)=>{
+    e.preventDefault()
+    console.log();
+    
+    
+  }
   return (
     <div>
-      
+      <form action="" onSubmit={(e)=>{
+        submith(e)
+      }}>
+        <input type="text" name="" placeholder ="  enter your name"id="" />
+        
+        <button> submit </button>
+      </form>
     </div>
   )
 }
