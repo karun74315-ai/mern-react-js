132b15b224883630c4eac8f62cb2c368a4bcf958
Arun Kumar <karun74315@gmail.com>
2026-08-01 16:15:43 +0530

    api calling using fetch

--- 0-15_API_calling/src/App.jsx
+++ 0-15_API_calling/src/App.jsx
@@ -1,20 +1,25 @@
 import React from 'react'
 
+
 function App() {
+  
+
   const getdata = async () => {
     const res = await fetch('https://jsonplaceholder.typicode.com/posts')
     const data = await res.json()
     console.log(data);
+  }
     
   }
   return (
     <div>
-      <button onClick={()=>{
-        getdata()
+      <button onClick={() => {
+
+         getdata()
       }}> Get Data </button>
       
     </div>
   )
-}
+
 
 export default App
