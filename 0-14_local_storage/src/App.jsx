cdb3a9f6577108e643602100ce8b362f7de05aa7
Arun Kumar <karun74315@gmail.com>
2026-08-01 15:32:56 +0530

    changes

--- 0-14_local_storage/src/App.jsx
+++ 0-14_local_storage/src/App.jsx
@@ -1,6 +1,24 @@
 import React from 'react'
 
 function App() {
+              // localStorage.setItem('age','18')
+//  const user = localStorage.getItem("name")
+//  const age = localStorage.getItem('age')
+//  console.log(user);
+//  console.log(age)
+
+//  const userr = {
+//   namee : "arun",
+//   umar : 9,
+//   city : "hp" 
+//  }
+ localStorage.setItem('user', JSON.stringify(userr))
+
+  const userr = JSON.parse(localStorage.getItem('user'))
+
+  console.log(userr);
+  
+ 
   return (
     <div>
       
